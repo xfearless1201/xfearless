0408659e00d57eeba2d7fe97241ae8633efbaf0e
hardy18723 <xfearless1201@gmail.com>
2019-05-27 12:43:42 +0800

    初始版本

---
+++ src/main/java/com/cn/xfearless/Bootstrap.java
@@ -0,0 +1,16 @@
+package com.cn.xfearless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * @author: xfearless
+ */
+@SpringBootApplication
+public class Bootstrap {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bootstrap.class, args);
+	}
+}
